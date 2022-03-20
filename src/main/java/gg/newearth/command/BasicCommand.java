1b0dbf923e6f3c22d482bcf14c3410f239f19492
imirochi <me@irochi.moe>
2022-03-20 17:37:45 +0900

    Added website command, Clickable links

--- src/main/java/gg/newearth/command/BasicCommand.java
+++ src/main/java/gg/newearth/command/BasicCommand.java
@@ -1,6 +1,8 @@
 package gg.newearth.command;
 
 import gg.newearth.Main;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.event.ClickEvent;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -14,22 +16,25 @@ public class BasicCommand implements CommandExecutor {
         switch (cmd.getLabel().toLowerCase()) {
             case "recipes" -> {
                 sender.sendMessage(Main.PREFIX + "§eYou can find our custom recipes at");
-                sender.sendMessage("§bhttps://newearth.gg/recipes");
+                sender.sendMessage(Component.text("§bhttps://newearth.gg/recipes").clickEvent(ClickEvent.openUrl("https://newearth.gg/recipes")));
             }
             case "store" -> {
                 sender.sendMessage(Main.PREFIX + "§eYou can buy ranks at");
-                sender.sendMessage("§bhttps://newearth.tebex.io/category/2011099");
+                sender.sendMessage(Component.text("§bhttps://newearth.tebex.io/category/2011099").clickEvent(ClickEvent.openUrl("https://newearth.tebex.io/category/2011099")));
             }
-            case "rules", "rule" -> {
+            case "rules" -> {
                 sender.sendMessage(Main.PREFIX + "§eYou can find our rules at");
-                sender.sendMessage("§bhttps://newearth.gg/rules");
+                sender.sendMessage(Component.text("§bhttps://newearth.gg/rules").clickEvent(ClickEvent.openUrl("https://newearth.gg/rules")));
             }
             case "map" -> {
                 sender.sendMessage(Main.PREFIX + "§eExplore map at");
-                sender.sendMessage("§bhttps://newearth.gg/map");
+                sender.sendMessage(Component.text("§bhttps://map.newearth.gg").clickEvent(ClickEvent.openUrl("https://map.newearth.gg")));
+            }
+            case "website" -> {
+                sender.sendMessage(Main.PREFIX + "§eOur Website!");
+                sender.sendMessage(Component.text("§bhttps://newearth.gg").clickEvent(ClickEvent.openUrl("https://newearth.gg")));
             }
         }
-
-        return true; // TODO: Add custom /help, /commands, /vote
+        return true; // TODO: Add custom /help, /commands
     }
 }
